412b22bc56194c903d5bfd85cecc582bed724a71
umardrazdotnetnisum <udraz@nisum.com>
2025-07-09 15:17:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,13 +22,13 @@ with st.expander('Data Visulizer'):
 
 # Data preparations
 with st.sidebar:
-    st.header('Input features')
-    island = st.selectbox('Island', ['Biscoe', 'Dream', 'Torgersen'])
-    bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
-    bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
-    flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
-    body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
-    gender = st.selectbox('Gender', ['male', 'female'])
+ st.header('Input features')
+ island = st.selectbox('Island', ['Biscoe', 'Dream', 'Torgersen'])
+ bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+ bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+ flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+ body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+ gender = st.selectbox('Gender', ['male', 'female'])
 
 # Create a DataFrame for the input features
 data = {
@@ -43,8 +43,8 @@ input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
 
 with st.expander('Input features'):
-    st.write('**Input penguin:**')
-    input_df
-    st.write('**Combined data:**')
+ st.write('**Input penguin:**')
+ input_df
+ st.write('**Combined data:**')
     input_penguins
 

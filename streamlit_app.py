256ed5b0b6a50755750a416f5859d31b3a968169
umardrazdotnetnisum <udraz@nisum.com>
2025-07-09 15:32:03 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,26 +47,26 @@ with st.expander('Input features'):
  input_df
  st.write('**Combined data:**')
  input_penguins
- # Data preparation
- # Encode X
- encode = ['island', 'sex']
- df_penguins = pd.get_dummies(input_penguins, prefix=encode)
- input_row = df_penguins[:1]
- # Encode y
- target_mapper = {'Adelie': 0,
+
+ 
+# Data preparation
+# Encode X
+encode = ['island', 'sex']
+df_penguins = pd.get_dummies(input_penguins, prefix=encode)
+input_row = df_penguins[:1]
+# Encode y
+target_mapper = {'Adelie': 0,
                  'Chinstrap': 1,
                  'Gentoo': 2}
 
- def target_encode(val):
+def target_encode(val):
     return target_mapper[val]
-
- y = y_raw.apply(target_encode)
-
+y = y_raw.apply(target_encode)
 with st.expander('Data preparation'):
-    st.write('**Encoded X (input penguin)**')
-    input_row
-    st.write('**Encoded y**')
-    y
+ st.write('**Encoded X (input penguin)**')
+ input_row
+ st.write('**Encoded y**')
+ y
 
 
 

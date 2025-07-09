2ec429b5896856906a4abbf91dfe1bf38d37e04f
umardrazdotnetnisum <udraz@nisum.com>
2025-07-09 14:10:04 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('UD ML 1st')
 
 st.write('Hellow ML Model')
+
+df = read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
